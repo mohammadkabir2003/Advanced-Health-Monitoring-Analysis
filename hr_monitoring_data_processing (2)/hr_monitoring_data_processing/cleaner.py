a83b072e6dd7d32b684534c695aca61fc23ba802
mohammadkabir2003 <115908951+mohammadkabir2003@users.noreply.github.com>
2025-03-28 23:56:24 -0400

    Delete hr_monitoring_data_processing (2)/hr_monitoring_data_processing directory

--- hr_monitoring_data_processing (2)/hr_monitoring_data_processing/cleaner.py
+++
@@ -1,9 +0,0 @@
-def filter_nondigits(data: list) -> list:
-    cleaned_data = []
-    for item in data:
-        item = item.strip("\n")
-        if item.isdigit():
-            cleaned_data.append(int(item))
-
-    return cleaned_data        
-    pass
